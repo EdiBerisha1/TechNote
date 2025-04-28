0da16d55c20904ce98d2ea56b1d76596eed70489
EdiBerisha1 <ediberishaa@outlook.com>
2025-04-28 14:03:42 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const fs = require("fs");
+const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+
+  try {
+    if (req.accepts("html")) {
+      const filePath = path.join(__dirname, "views", "404.html");
+      if (fs.existsSync(filePath)) {
+        return res.sendFile(filePath);
+      }
+      return res.send("<h1>404 Not Found</h1>");
+    }
+
+    if (req.accepts("json")) {
+      return res.json({ message: "404 Not Found" });
+    }
+
+    res.type("txt").send("404 Not Found");
+  } catch (err) {
+    console.error("Error in 404 handler:", err);
+    res.send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
